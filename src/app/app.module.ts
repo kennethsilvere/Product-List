bec68585f35f11e9e3798a696e0decdfadab0499
Kenneth Silvere <kennethsilvere93@gmail.com>
2017-10-18 20:07:31 -0400

    Add routes for navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductsComponent } from './products/products.component';
 import { AboutComponent } from './about/about.component';
+
+
+const appRoutes: Routes = [
+  { path:'', redirectTo:'products', pathMatch:'full' },
+  { path:'products', component: ProductsComponent },
+  { path:'about', component: AboutComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -16,7 +25,8 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
